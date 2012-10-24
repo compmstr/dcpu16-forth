2deae8906823ababad52fc7b31be919779602654
Corey <corey@undimk3>
2012-10-23 21:04:51 -0400

    Updated tokenva-size to take an a-mode flag Added tokenval-encode-LOC_... functions Added encode-tokenval Fixed replace-tokenval-label/s Added stubs for codelistentry-encode-<TYPE> Allocate space for and set up code to fill code buffer

--- compile.fs
+++ compile.fs
@@ -67,25 +67,27 @@ end-struct code-label
 		dup tokenval swap drop erase
 ;
 
-\ all of these: ( tokenval -- size )
-: tokenval-size-LOC_REG drop 0 ;
-: tokenval-size-LOC_MEM drop 1 ;
-: tokenval-size-LOC_REG_MEM drop 0 ;
-: tokenval-size-LOC_REG_MEM_OFFSET drop 1 ;
+\ all of these: ( a-mode tokenval -- size )
+: tokenval-size-LOC_REG 2drop 0 ;
+: tokenval-size-LOC_MEM 2drop 1 ;
+: tokenval-size-LOC_REG_MEM 2drop 0 ;
+: tokenval-size-LOC_REG_MEM_OFFSET 2drop 1 ;
 : tokenval-size-LOC_LITERAL
-		dup tokenval-val w@ 0x20 < if
+		dup tokenval-val w@ 0x20 <
+		\ and a-mode
+		and if
 				drop 0
 		else
 				drop 1
 		then
 ;
-: tokenval-size-LOC_SP drop 0 ;
-: tokenval-size-LOC_PC drop 0 ;
-: tokenval-size-LOC_EX drop 0 ;
-: tokenval-size-LOC_PUSHPOP drop 0 ;
-: tokenval-size-LOC_PEEK drop 0 ;
-: tokenval-size-LOC_PICK drop 1 ;
-: tokenval-size-LOC_LABEL drop 1 ;
+: tokenval-size-LOC_SP 2drop 0 ;
+: tokenval-size-LOC_PC 2drop 0 ;
+: tokenval-size-LOC_EX 2drop 0 ;
+: tokenval-size-LOC_PUSHPOP 2drop 0 ;
+: tokenval-size-LOC_PEEK 2drop 0 ;
+: tokenval-size-LOC_PICK 2drop 1 ;
+: tokenval-size-LOC_LABEL 2drop 1 ;
 
 create tokenval-sizers
 ' tokenval-size-LOC_REG ,
@@ -100,6 +102,78 @@ create tokenval-sizers
 ' tokenval-size-LOC_PEEK ,
 ' tokenval-size-LOC_PICK ,
 ' tokenval-size-LOC_LABEL ,
+
+\ all of these ( a-mode tokenval -- word [word] count )
+\ count is either 0 or 1, if there's an extra word
+: tokenval-encode-LOC_REG
+		swap drop tokenval-reg w@ 0
+;
+: tokenval-encode-LOC_MEM
+		swap drop tokenval-loc w@ \ loc
+		0x1e swap \ 0x1e loc
+		1
+;
+: tokenval-encode-LOC_REG_MEM
+		swap drop tokenval-reg w@ 0x08 + 0
+;
+: tokenval-encode-LOC_REG_MEM_OFFSET
+		swap drop dup \ loc loc
+		tokenval-reg w@ 0x10 + \ loc 0x10+reg
+		swap tokenval-loc w@ \ reg loc
+		1
+;
+: tokenval-encode-LOC_LITERAL
+		tokenval-val w@ \ a-mode val
+		swap over 0x20 < and if \ val
+				0x20 + 0 \ val+0x20 0
+		else
+				0x1f swap 1 \ 0x1f val 1
+		then
+;
+: tokenval-encode-LOC_SP
+		2drop 0x1b 0
+;
+: tokenval-encode-LOC_PC
+		2drop 0x1c 0
+;
+: tokenval-encode-LOC_EX
+		2drop 0x1d 0
+;
+: tokenval-encode-LOC_PUSHPOP
+		2drop 0x18 0
+;
+: tokenval-encode-LOC_PEEK
+		2drop 0x19 0
+;
+: tokenval-encode-LOC_PICK
+		swap drop \ loc
+		tokenval-loc w@ \ loc
+		0x1a swap 1 \ 0x1a loc 1
+;
+: tokenval-encode-LOC_LABEL
+		swap drop
+		tokenval-loc w@ 0x1f swap \ 0x1f(literal) loc
+		1
+;
+create tokenval-encoders
+' tokenval-encode-LOC_REG ,
+' tokenval-encode-LOC_MEM ,
+' tokenval-encode-LOC_REG_MEM ,
+' tokenval-encode-LOC_REG_MEM_OFFSET ,
+' tokenval-encode-LOC_LITERAL ,
+' tokenval-encode-LOC_SP ,
+' tokenval-encode-LOC_PC ,
+' tokenval-encode-LOC_EX ,
+' tokenval-encode-LOC_PUSHPOP ,
+' tokenval-encode-LOC_PEEK ,
+' tokenval-encode-LOC_PICK ,
+' tokenval-encode-LOC_LABEL ,
+
+: encode-tokenval ( tokenval -- word [word] count[0/1] )
+		dup tokenval-type w@ cells
+		tokenval-encoders + @
+		execute
+;
 
 \ returns the size in words of a token val
 \   either 0 or 1
@@ -119,12 +193,12 @@ create tokenval-sizers
 : get-codelistentry-size ( codelistentry -- size )
 		case dup codelistentry-type @
 				codelistentry-type_op of
-						dup codelistentry-aval @ get-tokenval-size
-						swap codelistentry-bval @ get-tokenval-size
+						true over codelistentry-aval @ get-tokenval-size
+						swap false swap codelistentry-bval @ get-tokenval-size
 						+ 1+
 				endof
 				codelistentry-type_special-op of
-						codelistentry-aval @ get-tokenval-size
+						true swap codelistentry-aval @ get-tokenval-size
 						1+
 				endof
 				codelistentry-type_label of
@@ -257,7 +331,7 @@ create tokenval-sizers
 ;
 
 \ runs through the code list, and sets the codeloc for each entry
-: set-codelist-codelocs ( -- ) 
+: set-codelist-codelocs ( -- size ) 
 		0 code-list @ \ <size> <first code entry>
 		begin
 				0 over <> while
@@ -270,33 +344,30 @@ create tokenval-sizers
 						\ get next entry
 						get-next-code-entry
 		repeat
-		2drop
+		swap drop \ size
 ;
 
 \ set a label type tokenval to a literal one
 : replace-tokenval-label ( tokenval -- )
 		dup tokenval-type @
-		LOC_LABEL over <> if
+		LOC_LABEL <> if
 				drop exit
 		then
 
 		dup tokenval-labelname @ \ val labelname
 		find-label \ val label
-		-1 over if
+		-1 over = if
 				." Error, label not found: "
 				dup tokenval-labelname @ print-string cr
 				abort
 		then
 		swap \ label val
-		LOC_LITERAL over tokenval-type w! \ label val
-		0 over tokenval-labelname w!
+		\ leave the type as label, just set the value
 		tokenval-val w!
 ;
 
 : replace-loc_labels  ( -- )
-		\ TODO fixme
 		code-list @ \ <first code entry>
-		\ break" replacing labels"
 		begin
 				0 over <> while
 						\ get entry's type
@@ -307,12 +378,12 @@ create tokenval-sizers
 										dup codelistentry-aval @
 										replace-tokenval-label
 										\ check if bval is label
-										codelistentry-bval @
+										dup codelistentry-bval @
 										replace-tokenval-label
 								endof
 								CODELISTENTRY-TYPE_SPECIAL-OP of
 										\ check if aval is label
-										codelistentry-aval @
+										dup codelistentry-aval @
 										replace-tokenval-label
 								endof
 						endcase
@@ -682,6 +753,24 @@ create tokenvalue-getters
 		then
 ;
 
+: codelistentry-encode-OP
+;
+: codelistentry-encode-SPECIAL-OP
+;
+: codelistentry-encode-LABEL
+;
+: codelistentry-encode-DATA
+;
+create codelistentry-encoders
+' codelistentry-encode-OP ,
+' codelistentry-encode-SPECIAL-OP ,
+' codelistentry-encode-LABEL ,
+' codelistentry-encode-DATA ,
+
+: encode-codelist ( buffer -- buffer )
+		code-list @ \ first thing of code
+;
+
 : compile-file ( string len -- )
 		open-input
 		begin
@@ -698,9 +787,12 @@ create tokenvalue-getters
 		drop \ drop last 0
 		close-input
 		\ calculate the code locations
-		set-codelist-codelocs
+		set-codelist-codelocs \ code-size
 		\ replace all of the locations set to labels with the label's position
 		replace-loc_labels
+		\ allocate space for the code
+		shorts allocate throw \ code-buffer
+		encode-codelist
 ;
 
 : test-compile
@@ -719,5 +811,4 @@ create tokenvalue-getters
 		read-input-line drop
 		process-line
 ;
-
 

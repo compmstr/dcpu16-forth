44fc48583baba63918b759ce7e85f8e29537817a
Corey <corey@undiwork>
2012-11-01 14:24:33 -0400

    Moved font stuff into hw directory Added screen vars

--- vm/hw/screen.fs
+++ vm/hw/screen.fs
@@ -1,8 +1,28 @@
 needs ../../utils/sdl.fs
+needs ../../utils/files.fs
 
-\ helper function to convert 4 bytes into a double word
-: convert-letter ( b b b b -- dw )
-		swap 8 shift-left +
-		swap 16 shift-left +
-		swap 24 shift-left +
-; immediate
+variable screen-cur-font
+0 screen-cur-font !
+variable screen-current-pallette
+0 screen-cur-pallete !
+variable screen-last-update
+0 screen-last-update !
+variable default-font
+0 default-font !
+
+: load-default-font
+;
+
+: screen-updater
+;
+: screen-hw-int-handler
+;
+: screen-get-hw-info
+;
+
+' screen-updater
+cr dup ." Screen Updater: " . cr
+' screen-hw-int-handler
+dup ." Screen Int-handler: " . cr
+' screen-get-hw-info
+dup ." Screen Info: " . cr
